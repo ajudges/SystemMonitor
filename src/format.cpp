ffed01377b53bfbb35fdeedd7a1ed419ef959c5d
nnamdi_ajah <nnamdi.ajah@simplifa.de>
2022-02-08 02:19:25 +0100

    fix time format

--- src/format.cpp
+++ src/format.cpp
@@ -6,7 +6,7 @@ using std::string;
 using std::to_string;
 
 string Format::ElapsedTime(long seconds) {
-  return to_string(int(seconds / 3600)) + ':' +
-         to_string(int((seconds / 60) % 60)) + ':' +
-         to_string(int(seconds % 60));
+  return to_string(long(seconds / 3600)) + ':' +
+         to_string(long((seconds / 60) % 60)) + ':' +
+         to_string(long(seconds % 60));
 }
